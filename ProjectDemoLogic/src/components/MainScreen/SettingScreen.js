212c683092d0545a6c6e38c1ad6f9db6a7d40bd0
TranDucLongDUT <longmup@gmail.com>
2018-09-03 11:44:45 +0700

    language v3.0

--- ProjectDemoLogic/src/components/MainScreen/SettingScreen.js
+++ ProjectDemoLogic/src/components/MainScreen/SettingScreen.js
@@ -7,23 +7,54 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View,ToastAndroid } from 'react-native';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 import I18N from '../../translations/languageControl'
+import I18nControl from 'react-native-i18n'
 
+var arrLanguages = [
+    {label: "English", value: 0},
+    {label: "VietNam", value: 1},
+]
 
 type Props = {};
 export default class App extends Component<Props> {
+
+    constructor(props) {
+        super(props);
+        this.state = props;
+        this.state = {language: "en"}
+    }
 
     static navigationOptions = {
         title: "Home ",
     }
     render() {
         const { params } = this.props.navigation.state;
+        I18nControl.locale = this.state.language;
         const myObj = I18N.t('mainscreen');
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Hello {params.username}</Text>
-                <Text style={styles.welcome}> {myObj.welcomeTitle}</Text>
+                {/* <Text style={styles.welcome}>Hello {params.username}</Text> */}
+                <Text style={styles.welcome}> {myObj.welcomeTitle} </Text>
+                <RadioForm 
+                    radio_props= {arrLanguages}
+                    initial = {0}
+                    onPress = {(value) => {
+                        if(value > 0) {
+                            this.setState({language: "vi"});
+                        } else {
+                            this.setState({language: "en"});
+                        }
+                    }}
+                    buttonSize = {25}
+                    selectedButtonColor = {'blue'}
+                    selectedLabelColor = {'blue'}
+                    labelStyle = {{fontSize : 20, }}
+                    formHorizontal = {false}
+                            
+                />
+
             </View>
         );
     }
